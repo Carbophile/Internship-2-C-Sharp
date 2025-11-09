9aa6e3ec663ff4660187ec21e093c7bd2166c68c
Borna Punda <borna@bornapunda.com>
2025-11-09 12:46:35 +0100

    feat(menu): menu shell

--- Internship-2-C-Sharp/Program.cs
+++ Internship-2-C-Sharp/Program.cs
@@ -2,8 +2,153 @@
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello, Internship-2-C-Sharp!");
+        while (true)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("1 - Korisnici");
+            Console.WriteLine("2 - Putovanja");
+            Console.WriteLine("0 - Izlaz iz aplikacije");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    UsersMenu();
+                    continue;
+                case "2":
+                    TripsMenu();
+                    continue;
+                case "0":
+                    return;
+            }
+            
+            Console.WriteLine("Neispravan unos, pokusajte ponovo!");
+        }
+    }
+
+    private static void UsersMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            
+            Console.WriteLine("1 - Unos novog korisnika");
+            Console.WriteLine("2 - Brisanje korisnika");
+            Console.WriteLine("3 - Uredjivanje korisnika");
+            Console.WriteLine("4 - Pregled svih korisnika");
+            Console.WriteLine("0 - Povratak na glavni izbornik");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AddUser();
+                    continue;
+                case "2":
+                    DeleteUser();
+                    continue;
+                case "3":
+                    EditUser();
+                    continue;
+                case "4":
+                    ListUsers();
+                    continue;
+                case "0":
+                    return;
+            }
+            
+            Console.WriteLine("Neispravan unos, pokusajte ponovo!");
+        }
+    }
+    
+    private static void AddUser()
+    {
+        Console.Clear();
+        Console.WriteLine("Stub - Add User");
+    }
+    
+    private static void DeleteUser()
+    {
+        Console.Clear();
+        Console.WriteLine("Stub - Delete User");
+    }
+    
+    private static void EditUser()
+    {
+        Console.Clear();
+        Console.WriteLine("Stub - Edit User");
+    }
+    
+    private static void ListUsers()
+    {
+        Console.Clear();
+        Console.WriteLine("Stub - List Users");
+    }
+
+    private static void TripsMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            Console.WriteLine("1 - Unos novog putovanja");
+            Console.WriteLine("2 - Brisanje putovanja");
+            Console.WriteLine("3 - Uređivanje postojećeg putovanja");
+            Console.WriteLine("4 - Pregled svih putovanja");
+            Console.WriteLine("5 - Izvještaji i analize");
+            Console.WriteLine("0 - Povratak na glavni izbornik");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    AddTrip();
+                    continue;
+                case "2":
+                    DeleteTrip();
+                    continue;
+                case "3":
+                    EditTrip();
+                    continue;
+                case "4":
+                    ListTrips();
+                    continue;
+                case "5":
+                    ReportsAndAnalysis();
+                    continue;
+                case "0":
+                    return;
+            }
+        }
+    }
+    
+    private static void AddTrip()
+    {
+        Console.Clear();
+        Console.WriteLine("Stub - Add Trip");
+    }
+    
+    private static void DeleteTrip()
+    {
+        Console.Clear();
+        Console.WriteLine("Stub - Delete Trip");
+    }
+    
+    private static void EditTrip()
+    {
+        Console.Clear();
+        Console.WriteLine("Stub - Edit Trip");
+    }
+    
+    private static void ListTrips()
+    {
+        Console.Clear();
+        Console.WriteLine("Stub - List Trips");
+    }
+    
+    private static void ReportsAndAnalysis()
+    {
+        Console.Clear();
+        Console.WriteLine("Stub - Reports and Analysis");
     }
 }
