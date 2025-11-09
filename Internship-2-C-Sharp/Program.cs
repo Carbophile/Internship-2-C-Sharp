1afe3e784071c2111ed678c0a7c67b24f52ede62
Borna Punda <borna@bornapunda.com>
2025-11-09 12:22:59 +0100

    chore(scaffold): initial commit

---
+++ Internship-2-C-Sharp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Internship_2_C_Sharp;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Internship-2-C-Sharp!");
+    }
+}
